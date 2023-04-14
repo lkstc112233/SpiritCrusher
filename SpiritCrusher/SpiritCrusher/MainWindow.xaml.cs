b307ae0bfd5cfd850a891e05f205d41c793ddcc6
Photon cat <lkstc112233@users.noreply.github.com>
2023-04-14 03:54:00 -0700

    I'm not sure what this is
    But I'm committing this repo and upload it anyway!

---
+++ SpiritCrusher/SpiritCrusher/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpiritCrusher
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // raised when the mouse pointer moves.
+        // Expands the dimensions of an Ellipse when the mouse moves.
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            // Get the x and y coordinates of the mouse pointer.
+            System.Windows.Point position = e.GetPosition(this);
+            double pX = position.X;
+            double pY = position.Y;
+            
+            Canvas.SetLeft(ellipse, pX - 50);
+            Canvas.SetTop(ellipse, pY - 50);
+        }
+    }
+}
